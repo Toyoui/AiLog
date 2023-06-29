28cdda58059db1fc750c714a61f9b718356a686f
Toyoui <91711069+Toyoui@users.noreply.github.com>
2023-06-29 23:04:49 +0800

    Update dockeroneclick.sh

--- dockeroneclick.sh
+++ dockeroneclick.sh
@@ -40,11 +40,13 @@ elif [[ $os_type == "ubuntu" ]]; then
     # ubuntu系统的安装脚本
     echo "正在安装Docker和相关组件（Ubuntu）..."
     sudo apt-get update
-    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
     sudo systemctl start docker
     sudo systemctl enable docker
 else
